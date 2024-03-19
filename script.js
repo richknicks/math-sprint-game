167ef65143367f220c6b592db58c7e4ef7f7d9b0
Richard Lynch <rlynch2012@gmail.com>
2024-03-19 01:18:04 -0400

    refactor code

--- script.js
+++ script.js
@@ -249,25 +249,27 @@ function populateGamePage() {
 
 // Displays 3, 2, 1, GO!
 function countdownStart () {
-  countdown.textContent = '3';
-  setTimeout(() => {
-    countdown.textContent = '2';
-  }, 1000);
-  setTimeout(() => {
-    countdown.textContent = '1';
-  }, 2000);
-  setTimeout(() => {
-    countdown.textContent = 'GO!';
-  }, 3000);
+  let count = 3;
+  countdown.textContent = count;
+  const timeCountDown = setInterval(() => {
+    count--;
+    if (count === 0) {
+      countdown.textContent = 'GO!'; 
+    } else if (count === -1) {
+      showGamePage();
+      clearInterval(timeCountDown);
+    } else {
+      countdown.textContent = count;
+    }
+  }, 1000)
 }
 
 // Navigate from Splash Page to Countdown Page
 function showCountdown () {
   countdownPage.hidden = false;
   splashPage.hidden = true;
+  populateGamePage(); 
   countdownStart();
-  populateGamePage();
-  setTimeout(showGamePage, 4000);
 }
 
 // Get the value from selected radio button
